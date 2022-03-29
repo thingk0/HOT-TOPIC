9a1dd64bae9bfc17f1749f9f1cc1ff607baa2faf
Thingk0 <audtjd9080@naver.com>
2022-03-29 13:58:12 +0900

    HotTopic - Project Start

---
+++ HotTopic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotTopic
+//
+//  Created by Thing K0 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
