670763510f7728efdf9dae242d98b8000fe9cff5
Thingk0 <audtjd9080@naver.com>
2022-04-06 15:16:11 +0900

    (+) NewsView -> Implemented news data in scroll view. (+) NewsDetail -> When you click a news in the list, thumbnails and brief news content are displayed, and when you click 'Shortcut', it moves to the original text of the news.

--- HotTopic/Cell.swift
+++ HotTopic/Cell.swift
@@ -1,43 +1,10 @@
 import UIKit
-import Alamofire
 
-class cell : UITableViewCell,UIWebViewDelegate {
+class cell : UITableViewCell {
     
+    
+
     @IBOutlet weak var labelText: UILabel!
-   
-    @IBOutlet weak var headLineImage: UIImageView!
     
-    var mainImageUrl : String?
     
-
-    func getImage (_ str: String?) {
-        
-        guard str != nil, let url = URL(string: str!) else {
-                                            
-            return
-            
-        }
-        
-        
-        if let data = try? Data(contentsOf: url), let img = UIImage(data: data) {
-            
-            DispatchQueue.main.async {
-                self.headLineImage.image = img
-                
-                
-                
-                
-            }
-        }
-    }
 }
-    
-
-    
-
-
-
-
-    
-    
-
--- HotTopic/NewsViewController.swift
+++ HotTopic/NewsViewController.swift
@@ -1,66 +1,61 @@
-//
-//  NewsViewController.swift
-//  HotTopic
-//
-
 import UIKit
-import Alamofire
 
-class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class NewsViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    // 몇 개를 셀에 표시할 것이냐?
     
-    // 몇 개를 셀에 표시할 것인가.
-    var newsData: [Dictionary<String, Any>]?
-    
+    var newsData : Array<Dictionary<String,Any>>?
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if let news = newsData {
+            
             return news.count
-        } else {
+            
+        }
+        
+        else {
+            
             return 0
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableViewMain.delegate = self
-        tableViewMain.dataSource = self
-        
-        // Naver API 호출 코드로 변경 X
-        requestAPIToNaver()
-        
-    }
+           // 셀에 무엇을 표시할 것이냐?
     
-    
-    // 셀에 무엇을 표시할 것인가?
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! cell
-        
+        let idx = indexPath.row
         if let news = newsData {
             
-            // r 은 각 셀의 인스턴스 -> 배열의 형태로 캐스팅 -> 셀은 재사용 -> 헤드라인 표시
-            let row = news[indexPath.row]
             
-            if let r = row as? [String: Any] {
+            // r은 각 셀의 인스턴스 -> 배열의 형태로 캐스팅 -> 셀은 재사용된다. -> 헤드라인 표시
+            
+            let row = news[idx]
+            if let r = row as? [String : Any] {
+            
                 if let title = r["title"] as? String {
+                    
                     cell.labelText.text = title
+                    
                 }
             }
         }
         
+        
         return cell
     }
     
-    // 클릭 시에 무엇을 할 것인가.
+
+    // 클릭시에 무엇을 할 것이냐
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "NewsDetailController") as! NewsDetailController
         
+       
     }
-    
     // 데이터 전달 (이미지 ,  헤드라인 , 세부내용 , url)
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let id = segue.identifier, "NewsDetail" == id {
             
@@ -72,7 +67,7 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
                         
                         let row = news[indexPath.row]
                         
-                           if let r = row as? Dictionary<String,Any> {
+                           if let r = row as? [String : Any] {
                             
                               if let imageUrl = r["urlToImage"] as? String {
                                   
@@ -87,7 +82,7 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
                                       if let detailUrl = r["url"] as? String {
                                 
                                             controller.detailUrl = detailUrl
-                            }
+                                  }
                         }
                     }
                 }
@@ -95,18 +90,20 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
     }
     
+    
     @IBOutlet weak var tableViewMain: UITableView!
     
     // 1. naver api를 사용한 데이터 호출
     // 2, newsapi를 사용한 데이터 호출
 
     func requestAPIToNaver(){
-        
+        // let getnewsapi : String = "https://newsapi.org/v2/top-headlines?country=kr&apiKey=2aaed997cd4b4081a73c53d9f585a52f"
         // navernews api :  articles 대신 items로 변경 "https://openapi.naver.com/v1/search/news.json?query=%EC%A3%BC%EC%8B%9D&display=20&start=1&sort=date"
         let clientID : String = "hkDjjW7uqkljwkHuXPAo"
         let clientKEY : String = "BstV_WDKLC"
         
-        let query : String = "https://openapi.naver.com/v1/search/news.json?query=%EC%A3%BC%EC%8B%9D&display=20&start=1&sort=date"
+        
+        let query : String = "https://newsapi.org/v2/top-headlines?country=kr&apiKey=2aaed997cd4b4081a73c53d9f585a52f"
         let encodedQuery : String = query.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
         let queryURL : URL = URL(string: encodedQuery)!
         
@@ -116,6 +113,7 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
             
+
             if let dataJson = data {
                 
                 do {
@@ -123,21 +121,43 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
                     let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary <String,Any>
                     
                     // 데이터 크롤링 naver api호출시 articles -> items로 변경 요청
-                    let items = json["items"] as! Array<Dictionary<String,Any>>
+                      
+                    let items = json["articles"] as! Array<Dictionary<String,Any>>
                     print(items)
                     
                     self.newsData = items
                     
                     // 메인스레드 호출 리로드 데이터를 통해 화면에 동기화 작업.
+                    
                     DispatchQueue.main.async {
                         self.tableViewMain.reloadData()
                     }
+                    
+                
                 }
                 
                 catch{}
+                
             }
+            
         }
-
+        
         task.resume()
+
+}
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableViewMain.delegate = self
+        tableViewMain.dataSource = self
+        
+        
+        // naver api 호출 코드로 변경x
+        requestAPIToNaver()
+       
     }
 }
+
+
